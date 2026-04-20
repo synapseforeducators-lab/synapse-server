b072b235206e2c79cae44d432a64a6bf834ecdda
Olagundoye Emmanuel <eoluwakayode34@gmail.com>
2026-04-20 09:54:21 +0100

    add documentation setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('setup-test')
+  getLoard(): string {
+    return this.appService.getHello();
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+        DB_PASS: Joi.string().required(),
+        PORT: Joi.number().required(),
+        DB_SYNCHRONIZE: Joi.boolean().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
