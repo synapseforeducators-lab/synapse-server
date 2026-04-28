0c30f98c039c90db991c2b4188cbddac6780b516
Olagundoye Emmanuel <eoluwakayode34@gmail.com>
2026-04-28 08:21:23 +0100

    add resend service

--- src/common/email/dto/create-email.dto.ts
+++ src/common/email/dto/create-email.dto.ts
@@ -1,1 +1,21 @@
-export class CreateEmailDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateEmailDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  from: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  to: string;
+
+  @IsString()
+  @IsNotEmpty()
+  subject: string;
+
+  @IsString()
+  @IsNotEmpty()
+  html: string;
+}
--- src/common/email/email.service.ts
+++ src/common/email/email.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create-email.dto';
 import { UpdateEmailDto } from './dto/update-email.dto';
+import { Resend } from 'resend';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EmailService {
-  create(createEmailDto: CreateEmailDto) {
-    return 'This action adds a new email';
+  private resend: Resend;
+
+  constructor(private readonly configService: ConfigService) {
+    const apiKey = this.configService.get<string>('RESEND_API_KEY');
+    this.resend = new Resend(apiKey);
   }
 
-  findAll() {
-    return `This action returns all email`;
-  }
+  async send(createEmailDto: CreateEmailDto) {
+    const { from, to, subject, html } = createEmailDto;
 
-  findOne(id: number) {
-    return `This action returns a #${id} email`;
-  }
-
-  update(id: number, updateEmailDto: UpdateEmailDto) {
-    return `This action updates a #${id} email`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} email`;
+    return await this.resend.emails.send({
+      from,
+      to,
+      subject,
+      html,
+    });
   }
 }
