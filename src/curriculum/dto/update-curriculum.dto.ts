034c526f0ab27a84cf274d99012cc27081380042
Olagundoye Emmanuel <eoluwakayode34@gmail.com>
2026-05-30 00:16:51 +0100

    update curriculum dto

--- src/curriculum/dto/update-curriculum.dto.ts
+++ src/curriculum/dto/update-curriculum.dto.ts
@@ -1,30 +1,37 @@
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsString, IsNotEmpty, IsArray, ValidateNested, IsOptional } from "class-validator";
-import { CreateCurriculumItemDto } from "./create-curriculum.dto";
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsString,
+  IsNotEmpty,
+  IsArray,
+  ValidateNested,
+  IsOptional,
+} from 'class-validator';
+import { CreateCurriculumItemDto } from './create-curriculum.dto';
 
+export class UpdateCurriculumDto {
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  name: string;
 
-export class UpdateCurriculumDto  {
-     @ApiPropertyOptional()
-      @IsString()
-      @IsOptional()
-      @IsNotEmpty()
-      name: string;
-    
-      @ApiProperty()
-      @IsString()
-      @IsNotEmpty()
-      subjectId: string;
-    
-      @ApiProperty()
-      @IsString()
-      @IsNotEmpty()
-      gradeId: string;
-    
-      @ApiProperty({ type: () => CreateCurriculumItemDto, isArray: true })
-      @IsArray()
-      @ValidateNested({ each: true })
-      @Type(() => CreateCurriculumItemDto)
-      @IsOptional()
-      items?: CreateCurriculumItemDto[] = [];
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  subjectId: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  gradeId: string;
+
+  @ApiProperty({ type: () => CreateCurriculumItemDto, isArray: true })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateCurriculumItemDto)
+  @IsOptional()
+  items?: CreateCurriculumItemDto[] = [];
 }
