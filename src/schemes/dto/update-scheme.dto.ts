8161770ceffce3525e184c63557e683548838b3f
Olagundoye Emmanuel <eoluwakayode34@gmail.com>
2026-06-02 19:22:40 +0100

    scheme update

--- src/schemes/dto/update-scheme.dto.ts
+++ src/schemes/dto/update-scheme.dto.ts
@@ -2,8 +2,4 @@ import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateSchemeDto } from './create-scheme.dto';
 import { IsString } from 'class-validator';
 
-export class UpdateSchemeDto extends PartialType(CreateSchemeDto) {
-  @ApiProperty()
-  @IsString()
-  id: string;
-}
+export class UpdateSchemeDto extends PartialType(CreateSchemeDto) {}
